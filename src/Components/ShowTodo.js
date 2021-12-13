5de13948f210d291c8cd7316cc11d3d40fec5a39
Mohdaqib1 <mohdaqib79656@gmail.com>
2021-12-13 12:34:36 +0530

    Mohd

--- src/Components/ShowTodo.js
+++ src/Components/ShowTodo.js
@@ -26,7 +26,14 @@ const ShowTodo = ({text,todo, todos, setTodos }) => {
                    <tr>
                         <td className={`todo-item ${todo.completed? "completed" : ""}`}>  {text}
                           <button  onClick={completeHandler} className="b2e" >   <img src={Edit} className="b2ed"/>  </button> 
-                           <button  onClick={deleteHandler} className="b2d">    <img src={Delete}  className="b2ed"/> </button> 
+                           <button  onClick={() => {
+    const confirmBox = window.confirm(
+      "Do you really want to delete this Crumb?"
+    )
+    if (confirmBox === true) {
+        deleteHandler()
+    }
+  }} className="b2d">    <img src={Delete}  className="b2ed"/> </button> 
                            </td>
                     </tr>
                </table>
